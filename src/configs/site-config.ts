0a3df656eeb92c78d36cbe74c6c2d69cdca72fb8
Emilio Navarro <sainfuw@outlook.com>
2024-08-19 21:41:27 -0400

    end of posts as pages

--- src/configs/site-config.ts
+++ src/configs/site-config.ts
@@ -2,36 +2,4 @@ export const siteConfig = {
   title: "Emilio's Blog",
   description: 'Sitio oficial de Emilio Navarro Web Developer & Web Designer',
   author: 'Emilio Navarro',
-  blogs: [
-    {
-      title: 'How to use sticky note for problem solving',
-      date: 'On: 20 October 2019',
-      image:
-        'https://images.unsplash.com/photo-1497032628192-86f99bcd76bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-    },
-    {
-      title: 'Morning routine to boost your mood',
-      date: 'On: 25 November 2020',
-      image:
-        'https://images.unsplash.com/photo-1544654803-b69140b285a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-    },
-    {
-      title: 'All the features you want to know',
-      date: 'On: 30 September 2020',
-      image:
-        'https://images.unsplash.com/photo-1530099486328-e021101a494a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1547&q=80',
-    },
-    {
-      title: 'Minimal workspace for your inspirations',
-      date: 'On: 13 October 2019',
-      image:
-        'https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1484&q=80',
-    },
-    {
-      title: 'What do you want to know about Blockchane',
-      date: 'On: 20 October 2019',
-      image:
-        'https://images.unsplash.com/photo-1624996379697-f01d168b1a52?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-    },
-  ],
-};
+}
