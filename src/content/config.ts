7fd0c2ff2fc06467fbe842e30bb37cfa04637890
Emilio Navarro <sainfuw@outlook.com>
2024-08-19 22:57:28 -0400

    section 06 ended

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+import { defineCollection, z } from 'astro:content'
+
+const blogCollection = defineCollection({
+  type: 'content',
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      description: z.string(),
+      author: z.string(),
+      image: image().refine((v) => v !== undefined, {
+        message: 'Image is required',
+      }),
+      tags: z.array(z.string()),
+    }),
+})
+
+export const collections = { blog: blogCollection }
